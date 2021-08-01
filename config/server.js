28b4d33321f1d0761a5b98d5d40bc2f57a630935
Lars Gerber <social@larsgerber.ch>
2021-08-01 15:52:30 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bf90b0dca47da30628224c40c982270e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2eec1af5-99e1-4314-a353-cd50eb5358ee'
+};
